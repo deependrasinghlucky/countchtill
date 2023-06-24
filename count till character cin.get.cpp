816b750e18c9639f1dc263702f872b92c3e505f3
Deependra Singh <108728913+deependrasinghlucky@users.noreply.github.com>
2023-06-24 10:12:42 +0530

    Add files via upload

---
+++ count till character cin.get.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+int main(){
+    
+    //count till character
+
+    // char ch;
+    // int co=0;
+    // cin>>ch;
+
+    // while(ch!='$'){
+    
+    // co++;
+    // cin>>ch;
+    // }
+
+    // cout<<co<<endl;
+
+    // cin ignores white spaces like \n \t or ' '
+    //so insted of using cin we use cin.get>>
+
+    // so now 
+
+    // syntex ch=cin.get();
+
+    
+//     char ch;
+//     int co=0;
+// ch=cin.get();
+
+//     while(ch!='$'){
+    
+//     co++;
+// ch=cin.get();
+//     }
+
+//     cout<<co<<endl;
+
+
+
+
+
+
+
+
+    return 0;
+}
